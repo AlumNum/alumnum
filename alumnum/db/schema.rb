8893e6cb65d11be9127b3d384fdd771ff2b33084
Caleb Savage <calebsavagephoto@gmail.com>
2016-02-10 13:45:45 -0500

    devise & schema

--- alumnum/db/schema.rb
+++ alumnum/db/schema.rb
@@ -11,10 +11,27 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160210175015) do
+ActiveRecord::Schema.define(version: 20160210181437) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "profiles", force: :cascade do |t|
+    t.string   "headline"
+    t.string   "image_url"
+    t.text     "bio"
+    t.integer  "view_count"
+    t.string   "status"
+    t.string   "twitter"
+    t.string   "github"
+    t.string   "peronal_site"
+    t.string   "linkedin"
+    t.datetime "created_at",   null: false
+    t.datetime "updated_at",   null: false
+    t.integer  "user_id"
+  end
+
+  add_index "profiles", ["user_id"], name: "index_profiles_on_user_id", using: :btree
 
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
@@ -29,9 +46,12 @@ ActiveRecord::Schema.define(version: 20160210175015) do
     t.inet     "last_sign_in_ip"
     t.datetime "created_at",                          null: false
     t.datetime "updated_at",                          null: false
+    t.string   "first_name"
+    t.string   "last_name"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
   add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
 
+  add_foreign_key "profiles", "users"
 end
