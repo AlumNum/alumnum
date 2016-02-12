b7e17cc9e0f3e96e9ef7e35acf137c81068d7177
Justin <jnorthageorr@gmail.com>
2016-02-12 16:27:10 -0500

    add admin index page, update profile show page

--- alumnum/app/controllers/static_controller.rb
+++ alumnum/app/controllers/static_controller.rb
@@ -1,8 +1,18 @@
 class StaticController < ApplicationController
+attr_accessor :id
 
 def index
-	@user = User.all
+	@user = User.page(params[:page]).per(15)
+	#@profiles = Profile.all
+	
+	#@profile = Profile.find(user_id=params[:id])
 end
+
+# def get_profile(user)
+# 	Profile.find(user_id=user)
+# end 
+
+
 
 
 end
