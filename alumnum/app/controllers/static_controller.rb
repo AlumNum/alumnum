2af85221f359e70ac6bfdc1cc00ec569cc6bf214
Justin <jnorthageorr@gmail.com>
2016-02-12 19:14:26 -0500

    admin status panel fixed, caleb's alum profile changes

--- alumnum/app/controllers/static_controller.rb
+++ alumnum/app/controllers/static_controller.rb
@@ -4,13 +4,11 @@ attr_accessor :id
 def index
 	@user = User.page(params[:page]).per(15)
 	#@profiles = Profile.all
-	
 	#@profile = Profile.find(user_id=params[:id])
+	binding.pry
 end
 
-# def get_profile(user)
-# 	Profile.find(user_id=user)
-# end 
+
 
 
 
