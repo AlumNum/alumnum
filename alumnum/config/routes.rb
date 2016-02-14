42c6217baa76cf36b230d63cf5b269d0b277906d
Caleb Savage <calebsavagephoto@gmail.com>
2016-02-14 13:20:30 -0500

    Search bar improved, add route for /u/:user_id which redirects to the profile of that user

--- alumnum/config/routes.rb
+++ alumnum/config/routes.rb
@@ -18,6 +18,7 @@ Rails.application.routes.draw do
    get '/search/history' => 'saved_searches#history'
    get '/search' => 'saved_searches#search' 
 
+   get '/u/:id' => 'profiles#find_by_user'
 
 
   # Example of regular route:
