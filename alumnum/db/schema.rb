66b7d165a0c6dc335e5e9e58d0883713aa9b78c5
Caleb Savage <calebsavagephoto@gmail.com>
2016-02-16 10:30:42 -0500

    I broke it

--- alumnum/db/schema.rb
+++ alumnum/db/schema.rb
@@ -11,9 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
 ActiveRecord::Schema.define(version: 20160215222750) do
-
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -38,16 +36,6 @@ ActiveRecord::Schema.define(version: 20160215222750) do
 
   add_index "follows", ["tag_id"], name: "index_follows_on_tag_id", using: :btree
   add_index "follows", ["user_id"], name: "index_follows_on_user_id", using: :btree
-
-  create_table "followtags", force: :cascade do |t|
-    t.integer  "user_id"
-    t.integer  "tag_id"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
-  add_index "followtags", ["tag_id"], name: "index_followtags_on_tag_id", using: :btree
-  add_index "followtags", ["user_id"], name: "index_followtags_on_user_id", using: :btree
 
   create_table "profiles", force: :cascade do |t|
     t.string   "headline"
@@ -165,8 +153,6 @@ ActiveRecord::Schema.define(version: 20160215222750) do
   add_foreign_key "answers", "questions"
   add_foreign_key "follows", "tags"
   add_foreign_key "follows", "users"
-  add_foreign_key "followtags", "tags"
-  add_foreign_key "followtags", "users"
   add_foreign_key "profiles", "users"
   add_foreign_key "qnas", "questions"
   add_foreign_key "qnas", "users"
